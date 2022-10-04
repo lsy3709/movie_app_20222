4deac26ac67da6565a6c22155dbfe4bb0a9fc3ee
lsy3709 <lsy3709@naver.com>
2022-10-05 08:24:33 +0900

    영화 api 사용해보기 axios 설치함.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('constructor 호출 hello');
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount 호출 component rendered');
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate 호출 I just updated');
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount 호출 goodbye, cruel world');
-  }
-
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-  
-      count: current.count - 1,
-    }));
-  };
-
-
+    isLoading: true,
+  }
+  componentDidMount(){
+    //영화 데이터 로딩!
+    setTimeout(()=>{
+      this.setState({isLoading : false});
+    },6000);
+  }
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading...':'We are ready'}</div>;
+    
   }
 }
 
