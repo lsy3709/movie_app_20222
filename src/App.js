691f6d04c7641382944d72db1b7c62def64617aa
lsy3709 <lsy3709@naver.com>
2022-10-05 06:49:33 +0900

    변경작업 확인중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
-function App() {
-  return  <div>Hello React</div>;
+
+class App extends React.Component {
+
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState(current => ({
+  
+      count: current.count - 1,
+    }));
+  };
+
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
